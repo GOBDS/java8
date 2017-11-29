c4a9ace1d80e24a1d307436b6ef973ed7ae73b15
Gabriel Oliveira Bernardo da Silva <gabriel_bernardo@outlook.com>
2017-11-29 15:52:51 -0200

    Method Reference

--- src/br/com/gobds/methodreference/MethodReference.java
+++ src/br/com/gobds/methodreference/MethodReference.java
@@ -1,9 +1,20 @@
 package br.com.gobds.methodreference;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class MethodReference {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List<String> palavras = new ArrayList<>();
+		palavras.add("alura online");
+		palavras.add("casa do código");
+		palavras.add("caelum");
+		
+		palavras.sort(Comparator.comparing(String::length));
+		
+		palavras.forEach(System.out::println);
 
 	}
 
