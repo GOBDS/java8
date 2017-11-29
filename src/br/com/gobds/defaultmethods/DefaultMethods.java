d2097db0364538660e912d102a90808696e82b26
Gabriel Oliveira Bernardo da Silva <gabriel_bernardo@outlook.com>
2017-11-29 10:15:39 -0200

    Default Methods

--- src/br/com/gobds/defaultmethods/DefaultMethods.java
+++ src/br/com/gobds/defaultmethods/DefaultMethods.java
@@ -1,10 +1,49 @@
 package br.com.gobds.defaultmethods;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class DefaultMethods {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		List<String> palavras = new ArrayList<>();
+		palavras.add("alura online");
+		palavras.add("casa do código");
+		palavras.add("caelum");
+		
+		Comparator<String> comparator = new Ordenador();
+		palavras.sort(comparator);
+		
+		Consumer<String> consumer = new PrintString();
+		palavras.forEach(consumer);
 
 	}
+	
 
+}
+
+class PrintString implements Consumer<String> {
+
+	@Override
+	public void accept(String s) {
+		System.out.println(s);
+		
+	}
+	
+}
+
+class Ordenador implements Comparator<String>{
+
+	@Override
+	public int compare(String s1, String s2) {
+		if (s1.length() < s2.length())
+            return -1;
+        if (s1.length() > s2.length())
+            return 1;
+		return 0;
+	}
+	
 }
