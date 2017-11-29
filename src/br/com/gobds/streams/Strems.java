61cf0f39a0f99c9735f1d33ca4eaa4a32839a135
Gabriel Oliveira Bernardo da Silva <gabriel_bernardo@outlook.com>
2017-11-29 16:45:08 -0200

    Streams

--- src/br/com/gobds/streams/Strems.java
+++ src/br/com/gobds/streams/Strems.java
@@ -1,10 +1,48 @@
 package br.com.gobds.streams;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Strems {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List<Curso> cursos = new ArrayList<Curso>();
+		cursos.add(new Curso("Python", 45));
+		cursos.add(new Curso("JavaScript", 150));
+		cursos.add(new Curso("Java 8", 113));
+		cursos.add(new Curso("C", 55));
+		
+		//Ordenar por numero de alunos
+		cursos.sort(Comparator.comparingInt(Curso::getAlunos));
+		cursos.forEach(s -> System.out.println(s.getNome()));
+		
+		//Filtar por numero de alunos
+		cursos.stream()
+			.filter(c -> c.getAlunos() >= 50)
+			.map(Curso::getNome)
+			.forEach(System.out::println);
+			
 
 	}
 
+}
+
+
+class Curso {
+    private String nome;
+    private int alunos;
+
+    public Curso(String nome, int alunos) {
+        this.nome = nome;
+        this.alunos = alunos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAlunos() {
+        return alunos;
+    }
 }
